609b19b85d06600b034ff1e4df3de6df7860c27b
zykee <3349567853@qq.com>
2017-06-24 03:37:59 +0800

    test8

---
+++ main/main.js
@@ -0,0 +1,75 @@
+module.exports = function main(input) {
+	var fig = new array(3*(input.length))
+	var text
+	for(var i=0;i<input.length;i++)
+	{
+		if(input[i]=='0')
+		{
+			fig[i]="._."
+			fig[i+input.length]="|.|"
+			fig[i+input.length*2]="|_|"
+		}
+		else if(input[i]=='1')
+		{
+			fig[i]="..."
+			fig[i+input.length]="..|"
+			fig[i+input.length*2]="..|"
+		}
+		else if(input[i]=='2')
+		{
+			fig[i]="._."
+			fig[i+input.length]="._|"
+			fig[i+input.length*2]="|_."
+		}
+		else if(input[i]=='3')
+		{
+			fig[i]="._."
+			fig[i+input.length]="._|"
+			fig[i+input.length*2]="._|"
+		}
+		else if(input[i]=='4')
+		{
+			fig[i]="..."
+			fig[i+input.length]="|_|"
+			fig[i+input.length*2]="..|"
+		}
+		else if(input[i]=='5')
+		{
+			fig[i]="._."
+			fig[i+input.length]="|_."
+			fig[i+input.length*2]="._|"
+		}
+		else if(input[i]=='6')
+		{
+			fig[i]="._."
+			fig[i+input.length]="|_'"
+			fig[i+input.length*2]="|_|"
+		}
+		else if(input[i]=='7')
+		{
+			fig[i]="._."
+			fig[i+input.length]="..|"
+			fig[i+input.length*2]="..|"
+		}
+		else if(input[i]=='8')
+		{
+			fig[i]="._."
+			fig[i+input.length]="|_|"
+			fig[i+input.length*2]="|_|"
+		}
+		else if(input[i]=='9')
+		{
+			fig[i]="._."
+			fig[i+input.length]="|_|"
+			fig[i+input.length*2]="..|"
+		}
+	}
+	for(var i=0;i<3*(input.length);i++)
+	{
+		console.log(fig[i]+" ")
+		if((i+1)%3==0)
+			console.log("\n")
+	}
+    console.log("Debug Info");
+    return 'Hello World!';
+};
