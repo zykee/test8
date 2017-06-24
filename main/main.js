5847cfb2c108fa861d1d419b47a7e244d8fd0531
zykee <3349567853@qq.com>
2017-06-24 10:53:28 +0800

    test8

--- main/main.js
+++ main/main.js
@@ -1,6 +1,6 @@
 module.exports = function main(input) {
 	var fig = new Array(3*(input.length))
-	var text
+	var text=''
 	var a
 	for(var i=0;i<input.length;i++)
 	{
@@ -67,10 +67,12 @@ module.exports = function main(input) {
 	}
 	for(var i=0;i<3*(input.length);i++)
 	{
-		console.log(fig[i]+" ")
+		text+=fig[i]
+		text+=' '
 		if((i+1)%3==0)
-			console.log("\n")
+			text+='\n'
 	}
+	console.log(text)
 	a=parseInt(input)
 	return a
     console.log("Debug Info");
